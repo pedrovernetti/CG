691b66e71a876b9b7dd671f8c8d9d151c63f2805
Pedro V G <pedrovernetti@gmail.com>
2018-04-04 08:44:27 -0300

    Launching the spacecraft!

---
+++ CMakeFiles/CG_UFPel.dir/DependInfo.cmake
@@ -0,0 +1,49 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hermit/Downloads/CG_UFPel/sources/controls.cpp" "/home/hermit/Downloads/CG_UFPel/CMakeFiles/CG_UFPel.dir/sources/controls.cpp.o"
+  "/home/hermit/Downloads/CG_UFPel/sources/glerror.cpp" "/home/hermit/Downloads/CG_UFPel/CMakeFiles/CG_UFPel.dir/sources/glerror.cpp.o"
+  "/home/hermit/Downloads/CG_UFPel/sources/main.cpp" "/home/hermit/Downloads/CG_UFPel/CMakeFiles/CG_UFPel.dir/sources/main.cpp.o"
+  "/home/hermit/Downloads/CG_UFPel/sources/objloader.cpp" "/home/hermit/Downloads/CG_UFPel/CMakeFiles/CG_UFPel.dir/sources/objloader.cpp.o"
+  "/home/hermit/Downloads/CG_UFPel/sources/quaternion_utils.cpp" "/home/hermit/Downloads/CG_UFPel/CMakeFiles/CG_UFPel.dir/sources/quaternion_utils.cpp.o"
+  "/home/hermit/Downloads/CG_UFPel/sources/shader.cpp" "/home/hermit/Downloads/CG_UFPel/CMakeFiles/CG_UFPel.dir/sources/shader.cpp.o"
+  "/home/hermit/Downloads/CG_UFPel/sources/tangentspace.cpp" "/home/hermit/Downloads/CG_UFPel/CMakeFiles/CG_UFPel.dir/sources/tangentspace.cpp.o"
+  "/home/hermit/Downloads/CG_UFPel/sources/text2D.cpp" "/home/hermit/Downloads/CG_UFPel/CMakeFiles/CG_UFPel.dir/sources/text2D.cpp.o"
+  "/home/hermit/Downloads/CG_UFPel/sources/texture.cpp" "/home/hermit/Downloads/CG_UFPel/CMakeFiles/CG_UFPel.dir/sources/texture.cpp.o"
+  "/home/hermit/Downloads/CG_UFPel/sources/vboindexer.cpp" "/home/hermit/Downloads/CG_UFPel/CMakeFiles/CG_UFPel.dir/sources/vboindexer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLEW_STATIC"
+  "TW_NO_DIRECT3D"
+  "TW_NO_LIB_PRAGMA"
+  "TW_STATIC"
+  "_CRT_SECURE_NO_WARNINGS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "external/AntTweakBar-1.16/include"
+  "external/glfw-3.0.3/include/GLFW"
+  "external/glm-0.9.4.0"
+  "external/glew-1.9.0/include"
+  "external/assimp-3.0.1270/include"
+  "include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/hermit/Downloads/CG_UFPel/external/glfw-3.0.3/src/CMakeFiles/GLFW_303.dir/DependInfo.cmake"
+  "/home/hermit/Downloads/CG_UFPel/external/CMakeFiles/GLEW_190.dir/DependInfo.cmake"
+  "/home/hermit/Downloads/CG_UFPel/external/CMakeFiles/ANTTWEAKBAR_116_OGLCORE_GLFW.dir/DependInfo.cmake"
+  "/home/hermit/Downloads/CG_UFPel/external/assimp-3.0.1270/code/CMakeFiles/assimp.dir/DependInfo.cmake"
+  "/home/hermit/Downloads/CG_UFPel/external/assimp-3.0.1270/contrib/zlib/CMakeFiles/zlib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
